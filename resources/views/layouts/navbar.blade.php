217a422b13d90924c05c777b9d2c8029ef9a7768
Marcos Evaldt <marcosevaldt@gmail.com>
2017-11-12 23:26:25 -0200

    Fixed Collapse navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse">
+  <div class="collapse navbar-collapse" id="navbarCollapse">
     @guest
     <ul class="navbar-nav mr-auto">
       <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
